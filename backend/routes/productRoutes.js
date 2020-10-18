1dadecdf237cac7a228d8ff2babe3a1f4305bf5f
maxdeale <maxericdeale@gmail.com>
2020-10-18 13:24:03 -0700

    starting auth routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,32 +1,15 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-
 const router = express.Router();
-
-import Product from "../models/productModel.js";
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js";
 
 //fetches all products from db, GET request to /api/products, publicly accesssed
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-
-    res.json(products);
-  })
-);
+router.route("/").get(getProducts);
 
 //fetch single product, GET request to /api/products, publicly accesssed
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product Not Found...");
-    }
-  })
-);
+
+router.route("/:id").get(getProductById);
 
 export default router;
